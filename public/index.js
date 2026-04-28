966e0d06064da2d6a0de374c320b29ea32098115
doctor112-1 <doctor112-1@proton.me>
2026-04-27 19:41:30 -0500

    change background from base to mantle

--- public/index.js
+++ public/index.js
@@ -15,7 +15,7 @@ socket.on("data", (msg) => {
 
 term.onTerminalReady = function () {
   const io = term.io.push()
-  term.setBackgroundColor("#1e1e2e")
+  term.setBackgroundColor("#181825")
   term.prefs_.set('cursor-color', 'f5e0dc')
   //term.prefs_.set('font-family', '"FiraCode Nerd Font", monospace');
   //term.prefs_.set('user-css', 'https://cdn.jsdelivr.net/gh/mshaugh/nerdfont-webfonts@v3.3.0/build/firacode-nerd-font.css');
